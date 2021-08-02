c7381a828b97e9f0de081738a493f420cd28f02b
alfredosebastiao <stiaoalfredo@gmail.com>
2021-08-02 23:53:14 +0200

    Project setup

---
+++ src/test/java/mz/alfredo/calculatorapi/CalculatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.alfredo.calculatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
